147305797bec8a872775e54c2c21142f7b4f97f9
peremateu <pere.mateu03@gmail.com>
2024-03-22 11:27:14 +0100

    obtenir dog-facts
    m'he carregat dog-images :)

---
+++ get-facts.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+def main():
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    params = {
+        "number": 10
+    }
+
+    url = "http://dog-api.kinduff.com/api/facts"
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        formatted_json = json.dumps(response.json(), indent=4)
+
+        with open("dog_facts.json", "w") as f:
+            f.write(formatted_json)
+        print("Dog facts data written to 'dog_facts.json'")
+
+    else:
+        print("Error:", response.status_code)
+   
+if __name__ == '__main__':
+    main()
+    
